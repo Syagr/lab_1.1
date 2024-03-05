8a6e5385207bfccd9e61cd5763c2fcc665a1491c
Pasha Siahrovskyi <114944986+Syagr@users.noreply.github.com>
2024-03-05 10:51:20 +0200

    change code by where actions

--- lab_1.1/Program.cs
+++ lab_1.1/Program.cs
@@ -15,12 +15,28 @@ public class Database
 
     public IEnumerable<Student> GetStudents() => _students;
 
-    public void PrintStudents()
+    public void PrintStudents(IEnumerable<Student> students)
     {
-        foreach (var student in _students)
+        foreach (var student in students)
         {
-            Console.WriteLine($"{student.Id}: {student.Name}, {student.Age}");
+            Console.WriteLine($"{student.Id}: {student.Name}, {student.Age}, {student.City}, {student.AverageScore}");
         }
+    }
+
+    public IEnumerable<Student> GetStudentsByAge(int minAge, int maxAge)
+    {
+        return _students.Where(s => s.Age >= minAge && s.Age <= maxAge);
+    }
+
+    public IEnumerable<Student> GetStudentsByCity(string city)
+    {
+        return _students.Where(s => s.City == city);
+        _students.FirstOrDefault(x=>x.Age>18)
+    }
+
+    public IEnumerable<Student> GetStudentsByAverageScore(double minScore)
+    {
+        return _students.Where(s => s.AverageScore >= minScore);
     }
 }
 
@@ -29,12 +45,16 @@ public class Student
     public int Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
+    public string City { get; set; }
+    public double AverageScore { get; set; }
 
-    public Student(int id, string name, int age)
+    public Student(int id, string name, int age, string city, double averageScore)
     {
         Id = id;
         Name = name;
         Age = age;
+        City = city;
+        AverageScore = averageScore;
     }
 }
 
@@ -44,12 +64,22 @@ class Program
     {
         var database = new Database();
 
-        // Додати тестових даних
-        database.AddStudent(new Student(1, "Petlyura", 15));
-        database.AddStudent(new Student(2, "Bandera", 16));
-        database.AddStudent(new Student(3, "Valodya_Zhelexyakus", 17));
+        database.AddStudent(new Student(1, "Petlyura", 15, "Kyiv", 4.5));
+        database.AddStudent(new Student(2, "Bandera", 16, "Lviv", 4.8));
+        database.AddStudent(new Student(3, "Valodya_Zhelexyakus", 17, "Kharkiv", 4.2));
+        database.AddStudent(new Student(4, "Taras_Bulba", 18, "Chernihiv", 4.0));
+        database.AddStudent(new Student(5, "Ivan_Franko", 19, "Ternopil", 4.9));
 
-        // Вивести список студентів
-        database.PrintStudents();
+        database.PrintStudents(database.GetStudents());
+
+
+        Console.WriteLine("\nStudents over the age of 18:");
+        database.PrintStudents(database.GetStudentsByAge(18, 18));
+
+        Console.WriteLine("\nStudents from the city of Kyiv:");
+        database.PrintStudents(database.GetStudentsByCity("Kyiv"));
+
+        Console.WriteLine("\nStudents with a grade point average above 4.5:");
+        database.PrintStudents(database.GetStudentsByAverageScore(4.5));
     }
 }
